877ae3811e3d1ef99dc8ff0d1ac4258d1f6aed2a
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2022-10-25 14:45:49 +0545

    Player Updated

--- core/src/plane/game/entities/Player.java
+++ core/src/plane/game/entities/Player.java
@@ -9,17 +9,23 @@ public class Player {
     Vector2 position = new Vector2(80, 80);
 
     PlayerIdle idle;
+    PlayerMove move;
 
     public void create() {
+        move = new PlayerMove(position);
         idle = new PlayerIdle(position);
+
         idle.create();
+        move.create();
     }
 
     public void render() {
-        idle.render();
+        // idle.render();
+        move.render();
     }
 
     public void dispose() {
         idle.dispose();
+        move.dispose();
     }
 }
